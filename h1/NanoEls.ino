d51b148d053deb6dca47aa034a69ba121703b02a
Maxim Kachurovskiy <slon.vsapogah@gmail.com>
2020-12-07 09:02:16 +0100

    Version 3

--- h1/NanoEls.ino
+++ h1/NanoEls.ino
@@ -34,7 +34,7 @@
 #define EEPROM_VERSION 1
 
 // To be incremented whenever a measurable improvement is made.
-#define SOFTWARE_VERSION 2
+#define SOFTWARE_VERSION 3
 
 // To be changed whenever a different PCB / encoder / stepper / ... design is used.
 #define HARDWARE_VERSION 1
